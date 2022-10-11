8ba3da388270e3065032b66539aaf0614437b437
root <noop@github.com>
2022-10-11 23:14:37 +0000

    Pages 49

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -5,8 +5,8 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "main.dart.js": "0f5a092c21820ef570a33292afb040a1",
-"index.html": "8c140debc208bfc8f4fafab7959d6e33",
-"/": "8c140debc208bfc8f4fafab7959d6e33",
+"index.html": "0e32adac77c02f427d653bdbfedb8b07",
+"/": "0e32adac77c02f427d653bdbfedb8b07",
 "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
 "manifest.json": "15f73b7e8a8209c2206210b3ac8dea1b",
 "canvaskit/canvaskit.js": "2bc454a691c631b07a9307ac4ca47797",
